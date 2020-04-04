d840d0d0f7ae29acc848263b25bd081290dde4f1
Murilo Campos <murilo13campos@gmail.com>
2020-04-04 15:58:36 -0400

    adding pie and line charts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import LineChartReport from './components/LineChart';
+import PizzaChartReport from './components/PizzaChart';
 
 function App() {
-  return <div />;
+  return (
+    <>
+      <LineChartReport />
+      <PizzaChartReport />
+    </>
+  );
 }
 
 export default App;
